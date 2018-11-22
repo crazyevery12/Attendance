fd1baaf62db784dd9d7efcc4f93a0f0b0c752987
Eric <anh.tran160201@vnuk.edu.vn>
2018-11-22 10:59:05 +0700

    database and model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('account_name');
+            $table->string('account_password');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthday');
+            $table->tinyinteger('gender');
+            $table->tinyinteger('position')->nullable();
+            $table->tinyinteger('status')->nulable();
             $table->rememberToken();
             $table->timestamps();
         });
