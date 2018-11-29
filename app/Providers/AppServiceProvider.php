1f6a76e733f92c0fcad2396bcab938de6dfb4bb5
ThangDang <tkpproo@gmail.com>
2018-11-29 08:49:57 +0700

    fix email validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
+use Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
         }
         DB::listen(function ($query) {
             \Log::info('QUERY: ' . $query->sql . 'TIME: ' . $query->time);
+        });
+        Validator::extend('email_input', function ($attr, $value){
+            return preg_match(' /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/', $value);
         });
     }
 
